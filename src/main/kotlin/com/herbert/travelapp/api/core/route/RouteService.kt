4e0f9f4f1c39e9a64c8f9ee8817f9408ed2a801d
Ian Patrick Herbert <ian@triliz.com>
2022-08-30 08:40:01 +0200

    Clean routeService

--- src/main/kotlin/com/herbert/travelapp/api/core/route/RouteService.kt
+++ src/main/kotlin/com/herbert/travelapp/api/core/route/RouteService.kt
@@ -5,8 +5,6 @@ import com.herbert.travelapp.api.core.city.CityProvider
 import com.herbert.travelapp.api.core.flight.FlightProvider
 import com.herbert.travelapp.api.core.station.StationProvider
 import com.herbert.travelapp.api.core.trainRoute.TrainRouteProvider
-import com.herbert.travelapp.api.utils.DistanceCalculator
-import com.herbert.travelapp.api.utils.Point
 import org.springframework.stereotype.Component
 
 @Component
@@ -20,25 +18,31 @@ class RouteService(
     override fun findAllRoutesFromCity(cityId: String): List<Route>? {
         val city = cityProvider.findCityById(cityId) ?: return null
         val connectedCities = cityProvider.findCitiesByAreaId(city.shareId!!)
-        val trainRoutes = stationProvider.findAllStationsByIdIn(connectedCities.flatMap { it.getStationIds() }.distinct()).map{ station ->
-            trainRouteProvider.getAllRoutesFromStation(station) ?: listOf()
-        }.flatten()
+        val trainRoutes =
+            stationProvider.findAllStationsByIdIn(connectedCities.flatMap { it.getStationIds() }.distinct())
+                .map { station ->
+                    trainRouteProvider.getAllRoutesFromStation(station) ?: listOf()
+                }.flatten()
 
         val flightRoutes = connectedCities.flatMap { it.getAirportIATACodes() }.distinct().mapNotNull { airport ->
-                val flights =  flightProvider.findAllFlightsFromAirport(airport)
+            val flights = flightProvider.findAllFlightsFromAirport(airport)
 
-                val airports = flights?.map {
-                    it.to!!.airportCode!!
-                }?.let{
-                    airportProvider.findAirportsByIATACode(it.plus(airport))
+            val destinationAirports = flights?.map {
+                it.to!!.airportCode!!
+            }.let {
+                airportProvider.findAirportsByIATACode(it!!.plus(airport))
+            }
+
+            flights?.mapNotNull { flight ->
+                val fromStop = destinationAirports?.find { it.iata == flight.from!!.airportCode }
+                val toStop = destinationAirports?.find { it.iata == flight.to!!.airportCode }
+                if(fromStop != null && toStop != null){
+                    flight.toRoute(toStop, fromStop)
+                }else{
+                    null
                 }
-
-                flights?.map { flight ->
-                    val fromStop = airports?.find{it.iata == flight.from!!.airportCode} ?: return null
-                    val toStop = airports.find{it.iata == flight.to!!.airportCode} ?: return null
-                    flight.toRoute(toStop,fromStop)
-                } ?: listOf()
-            }.flatten()
+            }
+        }.flatten()
         return listOf(trainRoutes, flightRoutes).flatten()
     }
 }
